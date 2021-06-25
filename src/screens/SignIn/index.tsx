9d6a3d01b52107b551be51bd7443c34a0a3ea6dc
dev7 <dev7@MacBook-Pro-de-macj-2.local>
2021-06-24 23:05:44 -0400

    first commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from 'react-native';
+
+export function SignIn() {
+
+    return (
+        <View>
+            <Text>
+                Primera App kkkkkkkkkkkkkk
+            </Text>
+
+        </View>
+
+    )
+
+}
