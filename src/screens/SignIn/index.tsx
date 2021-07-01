5bf2fa965fe0ea11b802969f93d72b9253478fc4
antoni perez <antoni.protoni@gmail.com>
2021-06-30 20:29:38 -0400

    add assets

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,16 +1,37 @@
-import React from "react";
-import { Text, View } from 'react-native';
+import React, {useState}from "react";
+import { Text, View , TextInput } from 'react-native';
+
+import { estilos } from "./styles";
 
 export function SignIn() {
 
+    const [textoInput, setTextoInput ] = useState('prueba');
+
     return (
-        <View>
-            <Text>
+        <View style={estilos.container}>
+            <Text >
                 Primera App kkkkkkkkkkkkkk
             </Text>
+
+            <TextInput style={estilos.input}
+            onChangeText={(valor) => setTextoInput(valor)}
+            
+            />
+
+            <Text>
+                Usted escribió .....{ textoInput }
+            </Text>
+
+            
 
         </View>
 
     )
 
 }
+
+
+
+
+
+{/* <View style={{ flex: 1, backgroundColor: 'red', alignItems:'center',justifyContent:'center'}}></View> */}
---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export const estilos = StyleSheet.create({
+  container:{
+      flex: 1,
+      justifyContent:'center',
+      backgroundColor:'grey',
+      alignItems:'center'
+     },
+  input:{
+      height:50,
+      width:200,
+      borderBottomWidth: 2,
+    
+  }  
+})
+
+
+
